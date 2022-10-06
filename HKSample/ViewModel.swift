ce2377eb09931ff3859d46b28dcd27aaf595a753
Isiah Malit <isiah.malit@publicissapient.com>
2022-10-06 13:00:29 -0400

    Wrapper class update

--- HKSample/ViewModel.swift
+++ HKSample/ViewModel.swift
@@ -7,163 +7,125 @@
 
 import Foundation
 import HealthKit
+import Combine
 
-class ViewModel: ObservableObject {
+enum ActivityType {
+    case mindfulness
+    case steps
+    case workout
+    case sleep
+}
+
+protocol HealthModule: AnyObject {
+    var healthStore: HKHealthStore? { get set }
+    var isHKAvailable: Bool { get }
+    func requestReadWritePermission(toShare: Set<HKSampleType>?,
+                                    toRead: Set<HKSampleType>?) -> Future<Bool, Error>
+    func getData(activityType: ActivityType) -> AnyPublisher<[HKSample]?, Error>
+}
+
+extension HealthModule {
+    var isHKAvailable: Bool {
+        return HKHealthStore.isHealthDataAvailable()
+    }
     
-    let healthStore = HKHealthStore()
-    var meditationData: [HKSample] = []
-    
-    func requestPermissions(){
-        let typestoRead = Set([
-            HKObjectType.quantityType(forIdentifier: .stepCount)!,
-            HKObjectType.categoryType(forIdentifier:.mindfulSession)!,
-            HKObjectType.workoutType(),
-            HKObjectType.categoryType(forIdentifier:.sleepAnalysis)!,
-            ])
+    func requestReadWritePermission(toShare: Set<HKSampleType>?,
+                                    toRead: Set<HKSampleType>?) -> Future<Bool, Error> {
         
-        let typestoShare = Set([
-            HKObjectType.quantityType(forIdentifier: .stepCount)!,
-            HKObjectType.categoryType(forIdentifier:.mindfulSession)!,
-            HKObjectType.workoutType(),
-            HKCategoryType.categoryType(forIdentifier: .sleepAnalysis)!
-            ])
+        let toShareTypes = getSampleTypes(sampleTypes: toShare)
+        let toReadTypes = getSampleTypes(sampleTypes: toRead)
         
-        self.healthStore.requestAuthorization(
-            toShare: typestoShare,
-            read: typestoRead
-        ) { (success, error) -> Void in
-            if success == false {
-                NSLog(" Display not allowed")
+        return Future { [weak self] promise in
+            self?.healthStore?.requestAuthorization(
+                toShare: toShareTypes,
+                read: toReadTypes
+            ) { authSuccess, error in
+                guard error == nil else {
+                    promise(.failure(error!))
+                    return
+                }
+                promise(.success(authSuccess))
             }
         }
     }
     
-    func getStepsData() {
+    func getData(activityType: ActivityType) -> AnyPublisher<[HKSample]?, Error> {
+        let subject = PassthroughSubject<[HKSample]?, Error>()
         
-        let calendar = Calendar.current
-        var anchorComponents = calendar.dateComponents([.day, .month,. year], from: Date())
-        anchorComponents.hour = 0
-        let anchorDate = calendar.date(from: anchorComponents)
-        
-        var interval = DateComponents()
-        interval.day = 1
-        
-        guard let quantityType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
-            print("nil step type")
-            return
+        //setup sample type
+        let sampleType = getSampleType(fromActivity: activityType)
+        guard let sampleType = sampleType else {
+            NSLog("No samples!")
+            subject.send(completion: .finished)
+            return subject.eraseToAnyPublisher()
         }
         
-        let query = HKStatisticsCollectionQuery(
-            quantityType: quantityType,
-            quantitySamplePredicate: nil,
-            options: .cumulativeSum,
-            anchorDate: anchorDate ?? Date(),
-            intervalComponents: interval
-        )
-        
-        query.initialResultsHandler = { query, results, error in
-            if error != nil {
-                print ("error in steps")
-            }
-            
-            results?.enumerateStatistics(
-                from: calendar.date(byAdding: .day, value: -7, to: Date()) ?? Date(),
-                to: Date()
-            ) { statResults, isStop in
-                print(statResults.sumQuantity())
-            }
-        }
-        
-        self.healthStore.execute(query)
-    }
-    
-    func getMindfulnessData() {
-        guard let mindfulType = HKObjectType.categoryType(forIdentifier: .mindfulSession) else {
-            print("nil mindfulness type")
-            return
-        }
+        //setup predicate and get data from 1 day before
+        let predicate = getPredicateOneDay()
         
         let query = HKSampleQuery(
-            sampleType: mindfulType,
-            predicate: nil,
+            sampleType: sampleType,
+            predicate: predicate,
             limit: 0,
             sortDescriptors: nil
-        ) { [weak self] query, samples, error in
-            if error != nil {
-                print("error in mindfulness")
+        ) { _, samplesFromQuery, error in
+            if let error = error {
+                subject.send(completion: .failure(error))
+                return
             }
             
-            if let samples = samples {
-                self?.displaySamples(samples: samples)
-                self?.meditationData = samples
-            }
-        }
-        
-        self.healthStore.execute(query)
-    }
-    
-    func getWorkoutData() {
-        let query = HKSampleQuery(
-            sampleType: HKObjectType.workoutType(),
-            predicate: nil,
-            limit: 0,
-            sortDescriptors: nil
-        ) { [weak self] query, samples, error in
-            if error != nil {
-                print("error in workout")
+            guard let samplesFromQuery = samplesFromQuery else {
+                subject.send(completion: .finished)
+                return
             }
             
-            if let samples = samples {
-                self?.displaySamples(samples: samples)
-            }
+            subject.send(samplesFromQuery)
+            subject.send(completion: .finished)
+            
         }
-        
-        self.healthStore.execute(query)
+
+        self.healthStore?.execute(query)
+        return subject.eraseToAnyPublisher()
+    }
+}
+
+private extension HealthModule {
+    func getSampleTypes(sampleTypes: Set<HKSampleType>?) -> Set<HKSampleType> {
+        if let customSampleTypes = sampleTypes {
+            return customSampleTypes
+        } else {
+            return Set([
+                    HKObjectType.quantityType(forIdentifier: .stepCount)!,
+                    HKObjectType.categoryType(forIdentifier:.mindfulSession)!,
+                    HKObjectType.workoutType(),
+                    HKObjectType.categoryType(forIdentifier:.sleepAnalysis)!,
+                ])
+        }
     }
     
-    func getSleepData() {
-        guard let sleepData = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) else {
-            print("nil sleep data")
-            return
+    func getSampleType(fromActivity: ActivityType) -> HKSampleType? {
+        var sampleType: HKSampleType?
+        switch fromActivity {
+        case .mindfulness:
+            sampleType = HKObjectType.categoryType(forIdentifier: .mindfulSession)
+        case .steps:
+            sampleType = HKObjectType.quantityType(forIdentifier: .stepCount)
+        case .workout:
+            sampleType = HKObjectType.workoutType()
+        case .sleep:
+            sampleType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)
         }
-        
-        //get sleep data from 1 day before
+        return sampleType
+    }
+    
+    func getPredicateOneDay() -> NSPredicate {
         let endDate = Date()
         let startDate = endDate.addingTimeInterval(-1.0 * 60 * 60 * 24)
         
-        let predicate = HKQuery.predicateForSamples(
+        return HKQuery.predicateForSamples(
             withStart: startDate,
             end: endDate,
             options: []
         )
-        
-        let query = HKSampleQuery(
-            sampleType: sleepData,
-            predicate: predicate,
-            limit: 0,
-            sortDescriptors: nil
-        ) { [weak self] query, samples, error in
-            if error != nil {
-                print("error in sleep")
-            }
-            
-            if let samples = samples {
-                self?.displaySamples(samples: samples)
-            }
-        
-        }
-        self.healthStore.execute(query)
-    }
-    
-    private func displaySamples(samples: [HKSample]) {
-        print(samples)
-        for sample in samples {
-            print(sample)
-        }
-    }
-    
-    func syncDisplay() {
-        print("syncing display")
-        getMindfulnessData()
     }
 }
